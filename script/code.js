3ae7a740941f5b66252a2d5f165029d5dc236fc9
KingBrink <kyle.brink1602@gmail.com>
2024-05-16 09:50:31 +0200

    joel

---
+++ script/code.js
@@ -0,0 +1,20 @@
+let numb1 = 2, numb2 = 4, numb3 = 5;
+// console.log(numb1 + numb2 * numb3)
+// console.log(numb3 % numb1)
+// console.log(numb3 ** numb1)
+// numb2 += numb1
+// numb2 = numb2 + numb1
+// console.log(numb2)
+// console.log(numb1 === numb2)
+// console.log(numb3 / numb1 > numb1)
+// console.log(numb2 == numb2 && numb3 < numb2)
+// console.log(typeof numb1)
+// let obj1 = new Number(12)
+let myArr = new Array()
+myArr.push(12, 'kyle')
+
+console.log(`numb1: ${numb1}`);
+console.log(obj1 instanceof Array);
+// console.log(obj1 instanceof Number);
+// console.log(typeof obj1);
+
